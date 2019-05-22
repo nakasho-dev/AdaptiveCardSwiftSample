4bbaf5eda2c0ea89ea2af525d66ee80cfe6894eb
Shinya Nakajima <19910245+nakasho-dev@users.noreply.github.com>
2019-05-23 00:28:50 +0900

    initial commit

---
+++ AdaptiveCardSwiftSample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AdaptiveCardSwiftSample
+//
+//  Created by Shinya Nakajima on 2019/05/08.
+//  Copyright © 2019 nakasho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AdaptiveCardSwiftSampleTests/AdaptiveCardSwiftSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  AdaptiveCardSwiftSampleTests.swift
+//  AdaptiveCardSwiftSampleTests
+//
+//  Created by Shinya Nakajima on 2019/05/08.
+//  Copyright © 2019 nakasho. All rights reserved.
+//
+
+import XCTest
+@testable import AdaptiveCardSwiftSample
+import Nimble
+
+class AdaptiveCardSwiftSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        expect(true).to(equal(true))
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
