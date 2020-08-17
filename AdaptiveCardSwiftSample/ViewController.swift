7c44318012aa862b7b334cc41658fd6bb8459844
nakasho <19910245+nakasho-dev@users.noreply.github.com>
2020-08-18 00:21:54 +0900

    AdaptiveCardsを2.0にしてみた

--- AdaptiveCardSwiftSample/ViewController.swift
+++ AdaptiveCardSwiftSample/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import BrightFutures
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let jsonStr = "{ \"type\": \"AdaptiveCard\", \"version\": \"1.0\", \"body\": [ { \"type\": \"Image\", \"url\": \"http://adaptivecards.io/content/adaptive-card-50.png\", \"horizontalAlignment\":\"center\" }, { \"type\": \"TextBlock\", \"horizontalAlignment\":\"center\", \"text\": \"Hello **Adaptive Cards!**\" } ], \"actions\": [ { \"type\": \"Action.OpenUrl\", \"title\": \"Learn more\", \"url\": \"http://adaptivecards.io\" }, { \"type\": \"Action.OpenUrl\", \"title\": \"GitHub\", \"url\": \"http://github.com/Microsoft/AdaptiveCards\" } ] }"
         
         view.backgroundColor = .red
         // Do any additional setup after loading the view.
