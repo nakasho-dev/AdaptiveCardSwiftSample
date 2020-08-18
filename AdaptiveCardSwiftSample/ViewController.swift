25704dad531c93fbdf96fe5e96326152b96db792
nakasho <19910245+nakasho-dev@users.noreply.github.com>
2020-08-19 02:37:19 +0900

    Whitesourceに出力されるか確認するために適当にライブラリを使ったコードを記述

--- AdaptiveCardSwiftSample/ViewController.swift
+++ AdaptiveCardSwiftSample/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 import BrightFutures
+import TinyConstraints
 
 class ViewController: UIViewController {
+    
+    let titleLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let jsonStr = "{ \"type\": \"AdaptiveCard\", \"version\": \"1.0\", \"body\": [ { \"type\": \"Image\", \"url\": \"http://adaptivecards.io/content/adaptive-card-50.png\", \"horizontalAlignment\":\"center\" }, { \"type\": \"TextBlock\", \"horizontalAlignment\":\"center\", \"text\": \"Hello **Adaptive Cards!**\" } ], \"actions\": [ { \"type\": \"Action.OpenUrl\", \"title\": \"Learn more\", \"url\": \"http://adaptivecards.io\" }, { \"type\": \"Action.OpenUrl\", \"title\": \"GitHub\", \"url\": \"http://github.com/Microsoft/AdaptiveCards\" } ] }"
         
-        view.backgroundColor = .red
+        view.backgroundColor = .white
         // Do any additional setup after loading the view.
+        
+        
+        
+        configSubviews()
+        addSubviews()
+        layoutSubviews()
     }
 
 
+    private func configSubviews() {
+        titleLabel.text = "テスト"
+    }
+    
+    private func addSubviews() {
+        view.addSubview(titleLabel)
+    }
+    
+    private func layoutSubviews() {
+        titleLabel.center(in: view)
+    }
+    
+    func asyncCalculation() -> Future<String, Never> {
+        return Future { complete in
+            DispatchQueue.global().async {
+                // do a complicated task and then hand the result to the promise:
+                complete(.success("forty-two"))
+            }
+        }
+    }
 }
 
